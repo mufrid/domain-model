9d7a9b0cc7f5aadc4abc6643fb2a2d4b04168f50
mufrid <mufridk@gmail.com>
2023-04-18 13:04:31 +0200

    Added new Membership project for modelling membership organizations. Removed warnings by upgrading to net7 target

---
+++ Membership/Membership.Types.fs
@@ -0,0 +1,18 @@
+﻿module MembershipTypes
+
+type MineAbonnement = Medlemsblad | Nyhetsbrev
+
+type PersonligInformasjon = PersonligInformasjon of string
+type Brukernavn = Brukernavn of string
+type Passord = Passord of string
+type Sikkerhet = { Brukernavn : Brukernavn; Passord : Passord }
+type MinProfil = { PersonligInfo : PersonligInformasjon; Sikkerhetspreferanser : Sikkerhet }
+
+type Faktura = Betalt | Ubetalt
+type Betaling = Bank | AvtaleGiro
+type Medlem = { Faktura : Faktura; Betaling : Betaling; MineAbonnement : MineAbonnement; MinProfil : MinProfil }
+
+
+
+
+
